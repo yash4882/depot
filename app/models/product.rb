93ad60f6c3fff2292f34f1358464b8595264cbb3
Yash Vasundhariya <yashvasundhariya.1@gmail.com>
2023-02-15 11:29:48 +0530

    validation and unit testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\z}i, message: 'must be a URL for GIF, JPG or PNG image.'
+}
+  # validates :title, :description, :image_url, presence: true 
 end
