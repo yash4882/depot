0cc5cf342e1fc360691b26338febb9b2d2df2f43
sahilshaikh7 <sahilshaikh3010@gmail.com>
2023-02-21 12:45:46 +0530

    half of send mail done

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    port:587,
+    domain:"domain.of.sender.net",
+    authentication: "plain",
+    user_name:"dave",
+    password:"123456",
+    enable_starttls_auto: true
+  }
+end
